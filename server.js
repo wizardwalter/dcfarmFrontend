da563973461c5f459a9ed5db0b335fede2c07873
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-12-14 21:40:48 -0500

    work please

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 var enforce = require('express-sslify');
 const app = express();
-app.use(enforce.HTTPS());
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 app.use(express.static(__dirname + '/dist/frontend'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname + '/dist/frontend/src/index.html'));});
