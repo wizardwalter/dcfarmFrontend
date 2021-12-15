a6161d96372aafe1408a85557ccbe35503c2ef9f
wizardwalter <67981837+wizardwalter@users.noreply.github.com>
2021-12-14 21:38:30 -0500

    work

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 
 const express = require('express');
-const sslRedirect =  require('heroku-ssl-redirect');
 const path = require('path');
+var enforce = require('express-sslify');
 const app = express();
+app.use(enforce.HTTPS());
 app.use(express.static(__dirname + '/dist/frontend'));
-app.use(sslRedirect());
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname + '/dist/frontend/src/index.html'));});
 app.listen(process.env.PORT || 8080);
